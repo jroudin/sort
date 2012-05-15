86cb9dd5f7f9b704b8f238cf67671f35425e5877
jr <jeanremy.oudin@gmail.com>
2012-05-15 23:18:38 +0200

    The bash script now has a debug option

--- run.sh
+++ run.sh
@@ -9,4 +9,7 @@ fi
 cat $1 | sort > /tmp/offisort;
 cat $1 | bin/sort > /tmp/mysort;
 diff /tmp/offisort /tmp/mysort;
-rm /tmp/offisort /tmp/mysort;
+if [ $# -lt 2 ]
+then
+	rm /tmp/offisort /tmp/mysort;
+fi
