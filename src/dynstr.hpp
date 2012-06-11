ffc375bef9f59a8054a37d27b41d00928ca7b080
jr <jeanremy.oudin@gmail.com>
2012-06-11 09:09:47 +0200

    Forgot to commit the isBlank prototype...

--- src/dynstr.hpp
+++ src/dynstr.hpp
@@ -13,6 +13,7 @@ struct DynStr
 DynStr* addChar(DynStr* str, const char c);
 void displayStr(DynStr* str);
 void freeStr(DynStr* str);
+bool isBlank(DynStr* s);
 int compareStr(DynStr* a, DynStr* b);
 
 #endif // DYN_STR_HPP
